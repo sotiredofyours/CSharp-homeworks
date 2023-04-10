a00d7820e0b5648f5e13f4c2ee8982c07b13cbe8
sotiredofyours <h8everrything@gmail.com>
2023-04-10 22:47:35 +0400

    Revert "ThreadPool: add IMyTask interface"
    This reverts commit e6a864b413f694b5fefffe3c8be9b97ebf453b8b.

--- MyThreadPool/IMyTask.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MyThreadPool;
-
-public interface IMyTask<TResult>
-{
-    public bool IsCompleted { get; }
-    public TResult Result { get; }
-    public IMyTask<TResult> ContinueWith(Func<TResult> func);
-}
